42e733afb5940d601f09a32f97f01cfe676d4877
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2018-09-26 18:28:51 -0300

    carga el socket desde component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { ChatService } from './chat.service';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,16 +8,4 @@ import { ChatService } from './chat.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ESP';
-  constructor(private chat: ChatService) {}
-
-// tslint:disable-next-line:use-life-cycle-interface
-ngOnInit() {
-  this.chat.messages.subscribe(msg => {
-    console.log(msg);
-  });
-}
-sendMessage() {
-  this.chat.sendMsg('Test Message');
-}
-}
+  title = 'ESP'; }
